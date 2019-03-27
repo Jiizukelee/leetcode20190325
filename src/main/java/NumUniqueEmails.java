e62120f37661c3741b5164b7dc46743842442c27
Jiizuke Lee <806777178@qq.com>
2019-03-27 09:57:54 +0800

    update

--- src/main/java/NumUniqueEmails.java
+++ src/main/java/NumUniqueEmails.java
@@ -1,18 +1,28 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class NumUniqueEmails {
-    public int numUniqueEmails(String[] emails){
-        for (String email:emails) {
+    public int numUniqueEmails(String[] emails) {
+        Set<String> set = new HashSet<>();
+//        String[] emailNew = new String[emails.length];
+        for (String email : emails) {
             String[] emailArr = email.split("@");
-
-
-
+            emailArr[0] = localName(emailArr[0]);
+            set.add(emailArr[0] + emailArr[1]);
         }
+        return set.size();
     }
 
 
-
-    public String localName(String name){
-        String replace = name.;
-
-
+    public String localName(String name) {
+        String localName;
+        if (name.contains("+")) {
+            localName = name.substring(0, name.indexOf("+")).replace(".", "");
+        } else {
+            localName = name.replace(".", "");
+        }
+        return localName;
     }
 }
+
+
